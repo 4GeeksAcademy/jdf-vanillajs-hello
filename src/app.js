109430f4b294fd00b80e5f992bdfe4fac456c66b
Juandomz91 <juandomz87@gmail.com>
2025-03-24 19:52:19 +0000

    liando la troca

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const randomnumber = Math.floor(Math.random()* length -1);
+console.log(randomnumber);
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  Math.random(Math.floor()*3)
+
+  console.log();
 };
