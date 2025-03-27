c1993ce5f885eb6bfa1056372702711e25e6329e
Juandomz91 <juandomz87@gmail.com>
2025-03-27 12:38:25 +0000

    añadiendo js

--- src/app.js
+++ src/app.js
@@ -10,10 +10,13 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-const myElement = document.getElementById('myElement')
-console.log(myElement)
-function randomnumber(length){
-  return Math.floor(Math.random * length -1);
+
+
+
+//const myElement = document.getElementById('myElement')
+//console.log(myElement)
+//function randomnumber(length){
+ // return Math.floor(Math.random * length -1);
 }
 
 
@@ -23,11 +26,19 @@ function getRandomPosition (){
 
 
 window.onload = function() {
- const randomnum = randomnumber();
+
+  
+const arrayElement = who[Math.floor(Math.random) * who.length];
+const arrayElement1 = action[Math.floor(Math.random) * action.length];
+const arrayElement2 = what[Math.floor(Math.random) * what.length];
+const arrayElement3 = when[Math.floor(Math.random) * when.length];
 
  const myElement = document.getElementById('myElement')
 
  myElement.innerHTML = randomnum
 
-  console.log();
+  console.log(arrayElement);
+  console.log(arrayElement1);
+  console.log(arrayElement2);
+  console.log(arrayElement3);
 };
